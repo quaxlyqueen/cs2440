8b9e730f4e7cdb9456754326ac303ef4cadff745
Joshua Ashton <joshuatashton+dev@gmail.com>
2025-01-15 10:03:08 -0700

    init commit

---
+++ birthday/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/styles.css">
+  <title>Birthday | Josh Ashton</title>
+</head>
+<body>
+
+  <?php
+    echo "
+      <div id=\"content\">
+        <div id=\"header-container\">
+          <h1>Happy Birthday!</h1>
+        </div>
+        <div id=\"pic-container\">
+          <img src=\"./img/birthday-AIgenerated.jpg\" />
+        </div>
+      </div>
+    ";
+  ?>
+
+</body>
+</html>
