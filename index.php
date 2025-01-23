51f5b41951d1d5e4307534f6232b1f46c9656456
Joshua Ashton <joshuatashton+dev@gmail.com>
2025-01-23 14:21:53 -0700

    testing ci/cd

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 <body>
   <h1>Josh Ashton's LAMPP page.</h1>
-  <p>Someday soon, this will be an awesome page!</p>
+  <p>Someday soonish, this will be an awesome page!</p>
   <a href="index.php">This link will help Jeff grade this assignment</a>
 </body>
 
