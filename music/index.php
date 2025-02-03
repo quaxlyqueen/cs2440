aa09c6128ccc6d661e737634f1b234a7d69dd01d
Josh Ashton <me@joshashton.dev>
2025-02-03 18:26:26 +0000

    fixing styling

--- music/index.php
+++ music/index.php
@@ -12,6 +12,7 @@ if (!isset($_GET['code'])) {
 <head>
     <title>Favorite Albums</title>
     <link rel="stylesheet" href="css/styles.css" type="text/css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="js/switch.js"></script>
 </head>
 
