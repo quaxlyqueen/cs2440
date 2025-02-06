fd3efd12260ba5f55dda76cfe42c9bed8000ae92
Joshua Ashton <joshuatashton+dev@gmail.com>
2025-02-06 13:16:23 -0700

    classwork

---
+++ 5min/day6/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Day 6</title>
+</head>
+
+<body>
+  <h1>Hello cruel world!</h1>
+  <form>
+    <?php
+    echo '<input type="text" placeholder="Lorem ipsum">
+    <input type="text" placeholder="dolor">';
+
+    ?>
+  </form>
+</body>
+
+</html
---
+++ exercises/4/foreach.php
@@ -0,0 +1,8 @@
+<?php
+$person = array(10 => 'beavis', 'jones');
+
+echo '<h4>';
+foreach ($person as $key => $p)
+    echo "<h4>$key - $p</h4>";
+echo '</h4>';
+?>
